4fa4e10bfb63019af53639261e53b2e599f57dec
LemonBoy <thatlemon@gmail.com>
2018-09-21 11:35:46 +0200

    Tidy up the json hierarchy

--- src/criterion/jsonExporter.nim
+++ src/criterion/jsonExporter.nim
@@ -12,10 +12,10 @@ proc `%`(r: BenchmarkResult): JsonNode =
   %*{ "label": r.label
     , "parameters": r.params
     , "data":
-      [ { "iterations":   r.stats.iterations }
-      , { "time_series":  r.stats.samples }
-      , { "cycle_series": r.stats.cycleSamples }
-      ]
+      { "iterations":   r.stats.iterations
+      , "time_series":  r.stats.samples
+      , "cycle_series": r.stats.cycleSamples
+      }
     }
 
 proc toJson*(cfg: Config, strm: Stream, r: BenchmarkResult) =
