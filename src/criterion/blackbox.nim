8bc805e7c53cb8b3cb39e2a6d3faff0caf9754db
LemonBoy <thatlemon@gmail.com>
2018-10-11 15:22:16 +0200

    blackBox for value types

--- src/criterion/blackbox.nim
+++ src/criterion/blackbox.nim
@@ -2,7 +2,13 @@ proc inner(x: ptr char) {.codegenDecl: "$# $#(char const volatile *x)".} =
   discard
 
 template blackBox*[T](x: var T) =
-  # Very simple (and far from zero-cost) harness that prevents the compiler from
-  # optimizing ``x`` away.
-  # At least it doesn't require any inline asm and is relatively portable.
+  ## Very simple (and far from zero-cost) harness that prevents the compiler
+  ## from optimizing ``x`` away.
+  ## At least it doesn't require any inline asm and is relatively portable.
   inner(cast[ptr char](unsafeAddr x))
+
+template blackBox*[T](x: T) =
+  ## Very simple (and far from zero-cost) harness that prevents the compiler
+  ## from optimizing ``x`` away.
+  ## At least it doesn't require any inline asm and is relatively portable.
+  inner(cast[ptr char](x))
